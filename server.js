d9491b9dfbaaaba2264bf15ab67543a95e918542
josue <utm22030755@utma.edu.mx>
2024-03-08 15:53:07 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json({limit:'50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
+
+app.listen(4000, ()=>{
+    console.log("Edgar Eduardo Rocha Ovalle")
+
+});
+
+export default app;
